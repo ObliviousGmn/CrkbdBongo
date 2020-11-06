de53725a18e78e769ef4deb171be7cc8962d43b9
ObliviousGmn <ObliviousGmn@users.noreply.github.com>
2020-11-06 13:11:26 -0500

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -69,13 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_WEAPON] = LAYOUT(
    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
+        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_1,   KC_2,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,   KC_6,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
+        KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_3,   KC_4,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,   KC_7,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EEP_RST,\
+        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_5,   KC_6,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EEP_RST,\
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_TAB,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS \
+                                         KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS \
                                         //`--------------------------'  `--------------------------'
     ),
 
