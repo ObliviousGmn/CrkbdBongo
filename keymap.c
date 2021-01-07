d49db8eceec2cb3cedabed31ab8ef27c3be4384f
ObliviousGmn <ObliviousGmn@users.noreply.github.com>
2021-01-07 12:13:00 -0500

    Just changed Gaming layer stuff...

--- keymap.c
+++ keymap.c
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_GAME] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,   KC_E,     KC_R,   KC_T,                         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,\
+       KC_TAB,    KC_Q,    KC_W,   KC_E,     KC_R,   KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,   KC_S,    KC_D,     KC_F,   KC_G,                         KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11, KC_F12,\
+      KC_LCTL,    KC_A,   KC_S,    KC_D,     KC_F,   KC_G,                          KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,   KC_X,    KC_C,     KC_V, KC_TRNS,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_WEAPON] = LAYOUT(
    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_1,   KC_2,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
+        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_1,   KC_2,                         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,\
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_3,   KC_4,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
+        KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_3,   KC_4,                         KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11, KC_F12,\
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_5,   KC_6,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EEP_RST,\
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
